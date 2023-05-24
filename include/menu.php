9c7244da05dcc8facdf9a6503c983c2626012122
Lokesh Sharma <lsharma87000@gmail.com>
2023-05-24 10:06:30 +0530

    portfolio

---
+++ include/menu.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg  main-nav" id="navbar">
+	<div class="container">
+	  <a class="navbar-brand" href="index.php">
+	  	<img src="images/logo.png" alt="" class="img-fluid">
+	  </a>
+
+	  <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="ti-align-justify"></span>
+	  </button>
+  
+		  <div class="collapse navbar-collapse" id="navbarsExample09">
+			<ul class="navbar-nav ml-auto">
+			  <li class="nav-item  active"><a class="nav-link" href="index.php">Home</a></li>
+
+			   <li class="nav-item  active"><a class="nav-link" href="about-us.php">About</a></li>
+
+			  <li class="nav-item  active"><a class="nav-link" href="index.php #services">Services</a></li>
+
+			    <li class="nav-item  active"><a class="nav-link" href="portfolio.php">Portfolio</a></li>
+
+			   <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+			</ul>
+		</div>
+	</div>
+</nav> 
